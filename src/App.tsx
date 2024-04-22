0ec65bce55bd6505691aa27653f7d502ac879d4a
TigerAW <adam.wan@hotmail.com>
2024-04-22 21:50:26 +0100

    image: added new image

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div>
         <a href="https://www.youtube.com/watch?v=by4SYYWlhEs" target="_blank">
-          <img src="https://media.discordapp.net/attachments/1218511095103356970/1232064502883680356/Screenshot_20240422_212109_Blue_Kik.jpg?ex=662818fc&is=6626c77c&hm=dd527455753549d43a68ccf0e4bd104f0e5265b8a09fe249184e50f845e2cca2&=&format=webp&width=1365&height=934" className="logo" alt="Vite logo" />
+          <img src="https://cdn.discordapp.com/attachments/1218511095103356970/1232070587166363811/RDT_20240416_022327739478396875606506.jpg?ex=66281ea6&is=6626cd26&hm=984d9c0fad948d9e1b4116e72c7b375900de823abc0ca72716bc7e0fb79e17e3&" className="logo" alt="Vite logo" height="1000"/>
         </a>
       </div>
       <h1>Who is this clown?</h1>
@@ -17,7 +17,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Learning React + Typescript
+          Personal Project For Learning React + Typescript
         </p>
       </div>
       <p className="read-the-docs">
