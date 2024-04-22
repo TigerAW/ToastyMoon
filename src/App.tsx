c46f2e37f221c6deae9cbbc7ddc45e00c79b1483
TigerAW <adam.wan@hotmail.com>
2024-04-22 21:32:40 +0100

    file: changes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +7,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.youtube.com/watch?v=by4SYYWlhEs" target="_blank">
+          <img src="https://media.discordapp.net/attachments/1218511095103356970/1232064502883680356/Screenshot_20240422_212109_Blue_Kik.jpg?ex=662818fc&is=6626c77c&hm=dd527455753549d43a68ccf0e4bd104f0e5265b8a09fe249184e50f845e2cca2&=&format=webp&width=1365&height=934" className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React + Toasty Test</h1>
+      <h1>Who is this clown?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Learning React + Typescript
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click the clown for the best song
       </p>
     </>
   )
